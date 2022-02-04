71e0afa58559474ac1d4ddd295dd3df1a69a3f96
Jakub <grabarczyk.kuba@gmail.com>
2022-02-04 17:51:42 +0100

    Fixing package weight and adding target message

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 package_count = 0
 package_weight = 0
+kilograms_sent = 0
 
 while package_count >= 0:
     print("Podaj wagę paczki")
@@ -7,16 +8,22 @@ while package_count >= 0:
     if element > 10 or element < 1:
         break
     else:
-        if package_weight >= 20:
+        if package_weight + element > 20:
             package_count += 1
+            kilograms_sent += package_weight
             package_weight = 0
             package_weight += element
             print("Paczka została wysłana")
         else:
             package_weight += element
 
-    print("Package count=", package_count)
-    print("Current package weight", package_weight)
+    print("Package count:", package_count)
+    print("Current package weight:", package_weight)
+    print("Kilograms sent:", kilograms_sent)
+
+print("1. Packages sent:", package_count)
+print("2. Kilograms sent:", kilograms_sent)
+print("3. Wasted kilograms:", (package_count * 20) - kilograms_sent)
 
 
 
