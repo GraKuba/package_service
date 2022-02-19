7deda08c8a6f90e8a9e6050e70b0e43ef48220df
Jakub <grabarczyk.kuba@gmail.com>
2022-02-19 16:59:26 +0100

    Program keeps track of the number of the package with the highest waste

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ package_weight = 0
 kilograms_sent = 0
 highest_waste = 0
 waste = 0
+highest_waste_package_nr = 1
+current_highest_waste_package_nr = 1
 
 while True:
     print("Input package weight")
@@ -11,12 +13,14 @@ while True:
         break
     else:
         if package_weight + element > 20:
+            highest_waste_package_nr += 1
             package_count += 1
             kilograms_sent += package_weight
             waste = 20 - package_weight
             if highest_waste < waste:
                 highest_waste = 0
                 highest_waste += waste
+                current_highest_waste_package_nr = highest_waste_package_nr
             package_weight = 0
             package_weight += element
             print("Package has been sent")
@@ -28,6 +32,5 @@ while True:
         else:
             package_weight += element
 
-
-print("Packages sent: {}, Kilograms sent: {}, Wasted kilograms: {}, Package with the highest waste: {}".format(
-    package_count, kilograms_sent, (package_count * 20) - kilograms_sent, highest_waste))
+print("Packages sent: {}, Kilograms sent: {}, Wasted kilograms: {}, Package with the highest waste: {} kg, nr. {}".format(
+    package_count, kilograms_sent, (package_count * 20) - kilograms_sent, highest_waste, current_highest_waste_package_nr))
