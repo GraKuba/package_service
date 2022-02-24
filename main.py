7d3e3eebb8d28b93963df294e589a099e6c6424f
Jakub <grabarczyk.kuba@gmail.com>
2022-02-24 13:26:14 +0100

    All mistakes fixed, program working for all options when package_count == 0

--- main.py
+++ main.py
@@ -10,6 +10,15 @@ while True:
     print("Input package weight")
     element = int(input())
     if element > 10 or element < 1:
+        package_weight += element
+        highest_waste_package_nr += 1
+        package_count += 1
+        kilograms_sent += package_weight
+        waste = 20 - package_weight
+        if highest_waste < waste:
+            highest_waste = 0
+            highest_waste += waste
+            current_highest_waste_package_nr = highest_waste_package_nr
         break
     else:
         if package_weight + element > 20:
@@ -38,17 +47,11 @@ while True:
             kilograms_sent += 20
             package_weight = 0
             print("Package has been sent")
-        elif package_weight < 20 and package_count == 0:
-            highest_waste_package_nr += 1
-            package_count += 1
-            kilograms_sent += element
-            waste = 20 - element
-            if highest_waste < waste:
-                highest_waste = 0
-                highest_waste += waste
-                current_highest_waste_package_nr = highest_waste_package_nr
         else:
             package_weight += element
+            print("Package weight: ", package_weight)
 
-print("Packages sent: {}, Kilograms sent: {}, Wasted kilograms: {}, Package with the highest waste: {} kg, nr. {}".format(
-    package_count, kilograms_sent, (package_count * 20) - kilograms_sent, highest_waste, current_highest_waste_package_nr))
+print(
+    "Packages sent: {}, Kilograms sent: {}, Wasted kilograms: {}, Package with the highest waste: {} kg, nr. {}".format(
+        package_count, kilograms_sent, (package_count * 20) - kilograms_sent, highest_waste,
+        current_highest_waste_package_nr))
